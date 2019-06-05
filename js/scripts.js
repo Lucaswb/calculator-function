8dd8d5449bf685e538af2e175e26127d8178a194
kelar crisp and Lucas Blasdel <lucas@>
2019-06-05 10:55:21 -0700

    add subtract multiply and divide

--- js/scripts.js
+++ js/scripts.js
@@ -15,11 +15,38 @@ var divide = function(number1, number2) {
 };
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("form#operator1").submit(function(event) {
     event.preventDefault();
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
     var result = add(number1, number2);
-    $("#output").text(result);
+    $("#output1").text(result);
+  });
+  $("form#operator2").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add3").val());
+    var number2 = parseInt($("#add4").val());
+    var result = subtract(number1, number2);
+    $("#output2").text(result);
+  });
+  $("form#operator3").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add5").val());
+    var number2 = parseInt($("#add6").val());
+    var result = multiply(number1, number2);
+    $("#output3").text(result);
+  });
+  $("form#operator4").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add7").val());
+    var number2 = parseInt($("#add8").val());
+    var result = divide(number1, number2);
+    $("#output4").text(result);
+  });
 });
-});
+  // $("form#operation").submit(function(event) {
+  //   event.preventDefault();
+  //   var number1 = parseInt($("#operation1").val());
+  //   var number2 = parseInt($("#operation2").val());
+  //   var result = subtract(number1, number2);
+  //   $("#output").text(result);
